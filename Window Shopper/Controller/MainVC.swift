10978c580557299c5905eda2c9b5155d4085002c
Pradhumna Panholi <user148657@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-08 20:02:09 -0500

    create "calculate button"

--- Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Controller/MainVC.swift
@@ -13,6 +13,17 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //for calculate button
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+    }
+    
+    @objc func calculate() {
+        print("It works")
     }
 
 
